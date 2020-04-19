9bdc45e3b04b430fd9c691d8193348e721f543d5
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2020-04-19 12:26:46 +0100

    Contact details

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -12,17 +12,19 @@ export default class Contact extends Component<{}, {}> {
 			<h2 className="centre-text">CONTACT US</h2>
 			<div className="contact-area">
 				<ul>
-					<li><FontAwesomeIcon icon={ faPhone } /><span>07816 920771 / 01623 431440</span></li>
-					<li><FontAwesomeIcon icon={ faEnvelope } /><span>abelectrical29@hotmail.com</span></li>
+					<li><FontAwesomeIcon icon={ faPhone } /><span><a href="tel:07816920771">07816 920771</a> / <a href="tel:01623431440">01623 431440</a></span></li>
+					<li><FontAwesomeIcon icon={ faEnvelope } /><span><a href="mailto:abelectrical29@hotmail.com">abelectrical29@hotmail.com</a></span></li>
 					<li><a href='#' onClick={ this.handleFacebookClick }><FontAwesomeIcon icon={ faFacebook } /><span>abelectricalnottinghamshire</span></a></li>
-					<li><FontAwesomeIcon icon={ faHome } /><div className="address-container">
-						<div>A. B. Electrical &amp; Maintenance</div>
-						<div>27 Greenbank Drive</div>
-						<div>Sutton-in-Ashfield</div>
-						<div>Nottinghamshire</div>
-						<div>NG17 2DY</div>
-						<div>United Kingdom</div>
-					</div></li>
+					<li><FontAwesomeIcon icon={ faHome } />
+						<div className="address-container">
+							<div>A. B. Electrical &amp; Maintenance</div>
+							<div>27 Greenbank Drive</div>
+							<div>Sutton-in-Ashfield</div>
+							<div>Nottinghamshire</div>
+							<div>NG17 2DY</div>
+							<div>United Kingdom</div>
+						</div>
+					</li>
 				</ul>
 				<iframe width="100%" height="355" frameBorder="0" scrolling="no" marginHeight={ 0 } marginWidth={ 0 } src="http://maps.google.co.uk/?ie=UTF8&amp;ll=53.1343522,-1.2652543&amp;spn=0.005694,0.016512&amp;t=m&amp;z=17&amp;output=embed" /><br />
 			</div>
