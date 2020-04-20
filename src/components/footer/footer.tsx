ae1d1b2354236e9ffbe31a26ab0ef8170cc593c5
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2020-04-20 11:17:46 +0100

    Footer update

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,10 +6,21 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import './footer.css';
 
 export default class Footer extends Component<{}, {}> {
+	private readonly thisYear: number;
+
+	constructor() {
+		super({});
+
+		const todaysDate = new Date();
+		this.thisYear = todaysDate.getFullYear();
+	}
+
 	public render() {
 		return <div className="footer-container">
 			<div className="centre-text">
-				&#169;2020 A B Electrical &amp; Maintenance | <a href="https://github.com/adrianeyre/a-b-electrical" target="_blank"><FontAwesomeIcon icon={ faGithub } /> Website Design</a>
+				<span>&#169;{ this.thisYear } A B Electrical &amp; Maintenance |</span>
+				<span className="extra-text"> All rights reserved | NAPIT REGISTERED &#38; TRUSTMARK |</span>
+				<span> <a href="https://github.com/adrianeyre/a-b-electrical" target="_blank"><FontAwesomeIcon icon={ faGithub } /> Website Design</a></span>
 			</div>
 		</div>
 	}
